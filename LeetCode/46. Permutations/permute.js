1f312b90ddbd6e024515405f66be30ba23be2304
berkansivri <berkansivri@gmail.com>
2019-05-22 13:09:18 +0300

    new solution

---
+++ LeetCode/46. Permutations/permute.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function(nums) {
+  const result = []
+  function iter(used) {
+    if (used.length === nums.length) result.push([...used])
+    else {
+      for (let num of nums) {
+        if (!used.includes(num)) {
+          used.push(num)
+          iter(used)
+          used.pop(num)
+        }
+      }
+    }
+  }
+  iter([])
+  return result
+}
