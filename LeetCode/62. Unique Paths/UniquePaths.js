1b1b5d57a46cdfbf57751326be1f8c7be4d55f65
Berkan <berkansivri@gmail.com>
2019-06-13 23:42:09 +0300

    new solution

---
+++ LeetCode/62. Unique Paths/UniquePaths.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  var arr = Array(m).fill([]).map(x => Array(n).fill(1))
+
+  for (var i = 1; i < m; i++) {
+    for (var j = 1; j < n; j++) {
+      arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+    }
+  }
+  return arr[m - 1][n - 1]
+};
+
+console.log(uniquePaths(3,3))
