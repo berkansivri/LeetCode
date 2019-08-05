be1dfc5eec474c4e801fe036c039a158d548a133
berkansivri <berkansivri@gmail.com>
2019-08-05 17:11:49 +0300

    .

---
+++ LeetCode/152. Maximum Product Subarray/MaxProduct.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let max = Number.MIN_SAFE_INTEGER, p = 1
+  for (let i = 0; i < nums.length; i++) {
+    max = Math.max(p *= nums[i], max)
+    if (nums[i] === 0) p = 1
+  }
+  p = 1
+  for (let i = nums.length - 1; i >= 0; i--) {
+    max = Math.max(p *= nums[i], max)
+    if (nums[i] === 0) p = 1
+  }
+  return max
+};
+
+console.log(maxProduct([2, -5, 8, 0, 2, -4, 3]));
+console.log(maxProduct([3, -1, 4]));
+console.log(maxProduct([-2, 0, -1])); // -1
+console.log(maxProduct([2, 3, -2, 4, -6]));
+console.log(maxProduct([-1, -1]));
---
+++ LeetCode/153. Find Minimum in Rotated Sorted Array/FindMin.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0,
+    right = nums.length - 1
+  while (left < right) {
+    var mid = Math.trunc(left + (right - left) / 2)
+    if (nums[mid] > nums[right])
+      left = mid + 1
+    else if (nums[mid] < nums[right])
+      right = mid
+  }
+  return nums[left]
+};
+
+console.log(findMin([1]));
+console.log(findMin([1, 2, 3, 4, 5]));
+console.log(findMin([3, 4, 5, 1, 2]));
+console.log(findMin([4, 5, 6, 7, 0, 1, 2]));
