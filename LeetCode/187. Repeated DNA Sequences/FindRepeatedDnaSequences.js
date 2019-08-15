71cf893ce6f2cc3dd9055d3f4e580dfe958fbf15
berkansivri <berkansivri@gmail.com>
2019-08-15 12:09:43 +0300

    .

---
+++ LeetCode/187. Repeated DNA Sequences/FindRepeatedDnaSequences.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function (s) {
+  let set = new Set()
+  for (let i = 0; i < s.length - 9; i++) {
+    let temp = s.substring(i, i + 10)
+    if (s.indexOf(temp, i + 1) > -1) {
+      if (!set.has(temp))
+        set.add(temp)
+    }
+  }
+  return Array.from(set)
+};
+
+console.log(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+console.log(findRepeatedDnaSequences("AAAAAAAAAAAA"));
