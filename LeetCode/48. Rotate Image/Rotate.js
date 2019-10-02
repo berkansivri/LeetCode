b476522f7e3233d68ef57fc2549380d11a85b3dd
berkansivri <berkansivri@gmail.com>
2019-10-02 09:38:03 +0300

    .

--- LeetCode/48. Rotate Image/Rotate.js
+++ LeetCode/48. Rotate Image/Rotate.js
@@ -2,7 +2,21 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function(matrix) {
+
+
+//O(1) space
+const rotate = (matrix) => {
+  const len = matrix.length
+  for (let i = 0; i < len / 2; i++) {
+    for (let j = i; j < len - i - 1; j++) {
+      [matrix[i][j], matrix[j][len - 1 - i], matrix[len - 1 - i][len - 1 - j], matrix[len - 1 - j][i]] = [matrix[len - 1 - j][i], matrix[i][j], matrix[j][len - 1 - i], matrix[len - 1 - i][len - 1 - j]]
+    }
+  }
+}
+
+
+//O(n) space
+var rotate2 = function (matrix) {
   var len = matrix.length
   var temp = JSON.parse(JSON.stringify(matrix))
 
