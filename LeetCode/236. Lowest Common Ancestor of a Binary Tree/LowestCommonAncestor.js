49f6784d5a548a9671d591e8e4d0c741365184d8
berkansivri <berkansivri@gmail.com>
2019-10-17 10:09:15 +0300

    .

---
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree/LowestCommonAncestor.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if(!root || root === p || root === q) return root
+  let left = lowestCommonAncestor(root.left, p, q)
+  let right = lowestCommonAncestor(root.right, p, q)
+  return (left && right) ? root : left ? left : right
+};
+
+let root = toBTree([3,5,1,6,2,0,8,null,null,7,4])
+let p = root.left.right
+let q = root.left.right.right
+
+console.log(lowestCommonAncestor(root, p, q))
+
+p = root.left
+q = root.right
+console.log(lowestCommonAncestor(root, p, q));
+
+function toBTree(array, i = 0) {
+  if (array[i] == null) return null
+  const root = new TreeNode(array[i])
+  root.left = toBTree(array, i * 2 + 1)
+  root.right = toBTree(array, i * 2 + 2)
+  return root
+}
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
