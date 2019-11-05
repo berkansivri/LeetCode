d02a400eee2e59451659df350bee284698f402fa
Berkan <berkansivri@gmail.com>
2019-11-05 22:46:44 +0300

    .

---
+++ LeetCode/279. Perfect Squares/NumSquares.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function (n) {
+  const dp = [0]
+  for (let i = 1; i <= n; i++) {
+    dp[i] = Number.MAX_SAFE_INTEGER
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1)
+    }
+  }
+  return dp[n]
+};
+
+console.log(numSquares(12));
+console.log(numSquares(13));
