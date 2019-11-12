66a806174a975b023cd82fa4ec5abfef36ec9ca3
berkansivri <berkansivri@gmail.com>
2019-11-12 10:21:46 +0300

    .

--- AddToReadmeExtension/extension.js
+++ AddToReadmeExtension/extension.js
@@ -1,33 +1,50 @@
 const vscode = require('vscode')
 const CreateFileAddReadMe = require('./CreateFileAddReadMe')
+const pushToRepo = require('./pushToRepo')
 
 function activate(context) {
+  console.log("ACTIVATED");
+  try {
+    let disposableReadme = vscode.commands.registerCommand('extension.addToReadme', async () => {
 
-  let disposable = vscode.commands.registerCommand('extension.addToReadme', async () => {
-
-    const question = await vscode.window.showInputBox({
-      placeHolder: "This will be folder name",
-      prompt: "Enter Question Title"
-    })
-    if (question.trim()) {
-      const solution = await vscode.window.showInputBox({
-        placeHolder: "This will be file name",
-        prompt: "Enter Solution Function Name"
+      const question = await vscode.window.showInputBox({
+        placeHolder: "This will be folder name",
+        prompt: "Enter Question Title"
       })
-      if (solution.trim()) {
-        const content = vscode.window.activeTextEditor.document.getText()
-        const folder = vscode.workspace.rootPath
-        try {
-          await CreateFileAddReadMe(question, solution, content, folder)
-          vscode.window.showInformationMessage("File Created and Added to ReadMe Table")
-        } catch (error) {
-          vscode.window.showWarningMessage(error.message)
+      if (question.trim()) {
+        const solution = await vscode.window.showInputBox({
+          placeHolder: "This will be file name",
+          prompt: "Enter Solution Function Name"
+        })
+        if (solution.trim()) {
+          const content = vscode.window.activeTextEditor.document.getText()
+          const folder = vscode.workspace.rootPath
+          try {
+            await CreateFileAddReadMe(question, solution, content, folder)
+            vscode.window.showInformationMessage("File Created and Added to ReadMe Table")
+          } catch (error) {
+            vscode.window.showWarningMessage(error.message)
+          }
         }
       }
-    }
-  })
+    })
 
-  context.subscriptions.push(disposable)
+    let disposableRepo = vscode.commands.registerCommand('extension.pushToRepo', async () => {
+      console.log("clicked pushtorepo");
+      const commitMsg = await vscode.window.showInputBox({
+        prompt: "Enter Commit Msg"
+      })
+      if (commitMsg) {
+        pushToRepo(commitMsg)
+      }
+    })
+
+
+    context.subscriptions.push(disposableReadme, disposableRepo)
+  } catch (error) {
+    vscode.window.showWarningMessage(error)
+  }
+
 }
 exports.activate = activate
 
---
+++ AddToReadmeExtension/pushToRepo.js
@@ -0,0 +1,31 @@
+const exec = require('child_process').exec
+const {
+  window
+} = require('vscode')
+
+const pushToRepo = (msg) => {
+  exec('git add .', (err, stdout, stderr) => {
+    if (err || stderr) {
+      console.log(err || stderr)
+      window.showErrorMessage(err || stderr)
+      return
+    }
+    exec(`git commit -am ${msg}`, (err, stdout, stderr) => {
+      if (err || stderr) {
+        console.log(err || stderr)
+        window.showErrorMessage(err || stderr)
+        return
+      }
+      exec('git push', (err, stdout, stderr) => {
+        if (err || stderr) {
+          console.log(err || stderr)
+          window.showErrorMessage(err || stderr)
+          return
+        }
+        window.showInformationMessage('Pushed to repository')
+      })
+    })
+  })
+}
+
+module.exports = pushToRepo
