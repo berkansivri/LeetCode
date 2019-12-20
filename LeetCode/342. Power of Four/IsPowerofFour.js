46edbf2a18796ce613bbad3592fe4169907e4736
berkansivri <berkansivri@gmail.com>
2019-12-20 14:03:28 +0300

    .

---
+++ LeetCode/342. Power of Four/IsPowerofFour.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function (num) {
+  let i = 1
+  while (i <= Number.MAX_SAFE_INTEGER) {
+    if (i === num) return true
+    else if (i > num) return false
+    i *= 4
+  }
+  return false
+};
+
+console.log(isPowerOfFour(2))
+console.log(isPowerOfFour(-2147483648))
