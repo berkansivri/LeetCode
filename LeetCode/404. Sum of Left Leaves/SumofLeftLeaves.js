3108e1f2cf9e434a2763dae6e08993028cca35f4
Berkan <berkansivri@gmail.com>
2020-02-10 14:04:30 +0300

    .

---
+++ LeetCode/404. Sum of Left Leaves/SumofLeftLeaves.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  if(!root) return 0
+  let sum = 0
+  const iter = (node, isLeft) => {
+    if(!node) return
+    if(isLeft && !node.left && !node.right) 
+      sum += node.val
+    iter(node.left, true)
+    iter(node.right, false)
+  }
+  iter(root)
+  return sum
+};
+
+function toBTree(array, i = 0) {
+  if (array[i] == null) return null
+  const root = new TreeNode(array[i])
+  root.left = toBTree(array, i * 2 + 1)
+  root.right = toBTree(array, i * 2 + 2)
+  return root
+}
+
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+
+console.log(sumOfLeftLeaves(
+  toBTree(
+    [0,2,4,1,null,3,-1,5,1,null,6,null,8]
+    )
+  )
+);
+console.log(sumOfLeftLeaves(
+  toBTree(
+      [3,9,20,null,null,15,7]
+    )
+  )
+);
