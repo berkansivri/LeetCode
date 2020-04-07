f6b3607046406cc8bff32c3acf84852831018763
berkansivri <berkansivri@gmail.com>
2020-04-08 00:30:30 +0300

    .

---
+++ LeetCode/- 30 Day LeetCoding Challenge/GroupAnagrams.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  let groups = [],
+    arr = []
+  for (let i = 0; i < strs.length; i++) {
+    arr = [strs[i]]
+    let s1 = strs[i].split("").sort().join()
+    for (let j = i + 1; j < strs.length; j++) {
+      if (strs[j].length === strs[i].length) {
+        let s2 = strs[j].split("").sort().join()
+        if (s1 === s2) {
+          arr.push(strs[j])
+          strs.splice(j, 1)
+          j--
+        }
+      }
+    }
+    groups.push(arr)
+  }
+  return groups
+};
---
+++ LeetCode/- 30 Day LeetCoding Challenge/MaxProfit.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0
+  for (let i = 1; i < prices.length; i++) {
+    if(prices[i] > prices[i-1])
+      profit += prices[i] - prices[i-1]
+  }
+  return profit
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([1, 2, 3, 4, 5]));
+console.log(maxProfit([1, 2, 3, 4, 5]));
