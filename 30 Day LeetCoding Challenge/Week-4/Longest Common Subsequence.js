57d8a1016837cd3b4a7768c72e25980f03b11b45
berkansivri <berkansivri@gmail.com>
2020-04-26 17:59:12 +0300

    1143

---
+++ 30 Day LeetCoding Challenge/Week-4/Longest Common Subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const dp = []
+  for (let i = 0; i <= text1.length; i++) {
+    dp[i] = []
+    for (let j = 0; j <= text2.length; j++) {
+      if(i === 0 || j === 0) {
+        dp[i][j] = 0
+      } else if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[text1.length][text2.length]
+}
+
+console.log(longestCommonSubsequence('abc', 'def'))
+console.log(longestCommonSubsequence('bl', 'yby'))
+console.log(longestCommonSubsequence('ezupkr', 'ubmrapg'))
+console.log(longestCommonSubsequence('abcde', 'ace'))
+console.log(longestCommonSubsequence('abc', 'abc'))
