812c2c249bc33d0d7eca1068dae78712bea184c2
berkansivri <berkansivri@gmail.com>
2020-05-04 00:03:51 +0300

    may week1 - 3

---
+++ May Leetcoding Challenge/Week-1/Ransom Note.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const chars = magazine
+    .split('')
+    .reduce((a, c) => ((a[c] = (a[c] || 0) + 1), a), {})
+
+  for (let l of ransomNote) {
+    if (chars[l]) chars[l]--
+    else return false
+  }
+  return true
+}
+
+console.log(canConstruct('a', 'b'))
+console.log(canConstruct('aa', 'ab'))
+console.log(canConstruct('aa', 'aab'))
