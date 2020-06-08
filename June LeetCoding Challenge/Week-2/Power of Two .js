b0b76d29e2dd7fed106760ee0e2803d2c347b716
berkansivri <berkansivri@gmail.com>
2020-06-08 17:28:19 +0300

    june 8

---
+++ June LeetCoding Challenge/Week-2/Power of Two .js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  return n > 0 ? !(n & (n - 1)) : false
+}
+
+console.log(isPowerOfTwo(1))
+console.log(isPowerOfTwo(16))
+console.log(isPowerOfTwo(218))
