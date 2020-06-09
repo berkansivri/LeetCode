a379231605951173d43e875d26ae02a65cba3e6d
berkansivri <berkansivri@gmail.com>
2020-06-09 14:19:57 +0300

    june 9

---
+++ June LeetCoding Challenge/Week-2/Is Subsequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let i = 0,
+    j = 0,
+    lens = s.length,
+    lent = t.length
+
+  if (lens === 0) return true
+  while (j < lent) {
+    if (s[i] === t[j]) {
+      i++
+      if (i === lens) return true
+    }
+    j++
+  }
+  return false
+}
+
+console.log(isSubsequence('abc', 'ahbgdc'))
+console.log(isSubsequence('axc', 'ahbgdc'))
