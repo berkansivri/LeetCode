a7081e76d38a8810b237d5343899208db9aeb380
berkansivri <berkansivri@gmail.com>
2020-12-12 22:12:14 +0300

    q.98 refactor

--- LeetCode/98. Validate Binary Search Tree/IsValidBST.js
+++ LeetCode/98. Validate Binary Search Tree/IsValidBST.js
@@ -9,13 +9,10 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function (root) {
-  const isValid = (root, min, max) => {
-    if (!root) return true
-    if (root.val >= max || root.val <= min) return false
-    return isValid(root.left, min, root.val) && isValid(root.right, root.val, max)
-  }
-  return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+var isValidBST = function (root, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) {
+  if (!root) return true
+  if (root.val >= max || root.val <= min) return false
+  return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max)
 }
 
 function TreeNode(val) {
