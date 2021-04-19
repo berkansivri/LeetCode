74f83003159b0ede90d11cd6477276cc912e67fb
berkansivri <berkansivri@gmail.com>
2021-04-19 23:57:52 +0300

    3

--- LeetCode/3. Longest Substring Without Repeating Characters/LengthOfLongestSubstring.js
+++ LeetCode/3. Longest Substring Without Repeating Characters/LengthOfLongestSubstring.js
@@ -2,23 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-  let substring = ''
-  let startIndex = 0
-  let temp = ''
+function findLongestSubstring(str) {
+  const seen = new Map()
+  let start = 0
+  let maxLen = 0
 
-  for (let i = 0; i < s.length; i++) {
-    temp += s[i]
-
-    if (substring.length < temp.length) {
-      substring = [...temp]
+  for (let i = 0; i < str.length; i++) {
+    if (seen.has(str[i])) {
+      start = Math.max(start, seen.get(str[i]) + 1)
     }
-
-    if (s[i + 1] && temp.includes(s[i + 1])) {
-      temp = ''
-      i = startIndex++
-      continue
-    }
+    seen.set(str[i], i)
+    maxLen = Math.max(maxLen, i - start + 1)
   }
-  return substring.length
+
+  return maxLen
 }
+
+console.log(findDiagonalOrder('abcabcbb'))
+console.log(findDiagonalOrder('bbbbbb'))
+console.log(findDiagonalOrder('pwwkew'))
+console.log(findDiagonalOrder(''))
