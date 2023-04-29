2f81dc7d7f6179973e0f8452d968c8df546faa03
Berkan Sivri <berkansivri@gmail.com>
2023-04-29 16:01:26 +0300

    1 two sum

--- LeetCode/1. Two Sum/TwoSum.js
+++ LeetCode/1. Two Sum/TwoSum.js
@@ -4,12 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  let map = {}
   for (let i = 0; i < nums.length; i++) {
-    var temp = nums[i]
-    for (let j = 0; j < nums.length; j++) {
-      if (i !== j && temp + nums[j] === target) {
-        return [i, j]
-      }
-    }
+    let diff = target - nums[i]
+    if (map[diff] !== undefined) return [map[diff], i]
+    map[nums[i]] = i
   }
 }
+
+console.log(twoSum([3, 3], 6))
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 3], 6))
